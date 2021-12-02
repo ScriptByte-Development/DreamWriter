79646a5879d4a24af0d0160ef28fac9fc357259c
Woke <66000596+woke999@users.noreply.github.com>
2021-12-01 20:21:14 -0500

    Add files via upload

---
+++ DreamWriter/UI/EditorWndw.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace DreamWriter
+{
+    partial class EditorWndw
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EditorWndw));
+            this.dragCtrl = new Siticone.UI.WinForms.SiticoneDragControl(this.components);
+            this.topPnl = new System.Windows.Forms.Panel();
+            this.minimizeBtn = new Siticone.UI.WinForms.SiticoneButton();
+            this.exitBtn = new Siticone.UI.WinForms.SiticoneButton();
+            this.CodeEditor = new System.Windows.Forms.WebBrowser();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.topPnl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dragCtrl
+            // 
+            this.dragCtrl.TargetControl = this.topPnl;
+            // 
+            // topPnl
+            // 
+            this.topPnl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(13)))), ((int)(((byte)(13)))));
+            this.topPnl.Controls.Add(this.pictureBox1);
+            this.topPnl.Controls.Add(this.minimizeBtn);
+            this.topPnl.Controls.Add(this.exitBtn);
+            this.topPnl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPnl.Location = new System.Drawing.Point(0, 0);
+            this.topPnl.Name = "topPnl";
+            this.topPnl.Size = new System.Drawing.Size(867, 32);
+            this.topPnl.TabIndex = 0;
+            // 
+            // minimizeBtn
+            // 
+            this.minimizeBtn.BackColor = System.Drawing.Color.Transparent;
+            this.minimizeBtn.BorderColor = System.Drawing.Color.Transparent;
+            this.minimizeBtn.BorderRadius = 4;
+            this.minimizeBtn.BorderThickness = 2;
+            this.minimizeBtn.CheckedState.Parent = this.minimizeBtn;
+            this.minimizeBtn.CustomBorderColor = System.Drawing.Color.Transparent;
+            this.minimizeBtn.CustomImages.Parent = this.minimizeBtn;
+            this.minimizeBtn.FillColor = System.Drawing.Color.Transparent;
+            this.minimizeBtn.Font = new System.Drawing.Font("Sitka Text", 11.25F);
+            this.minimizeBtn.ForeColor = System.Drawing.Color.White;
+            this.minimizeBtn.HoveredState.Parent = this.minimizeBtn;
+            this.minimizeBtn.Image = ((System.Drawing.Image)(resources.GetObject("minimizeBtn.Image")));
+            this.minimizeBtn.ImageSize = new System.Drawing.Size(12, 12);
+            this.minimizeBtn.Location = new System.Drawing.Point(768, 3);
+            this.minimizeBtn.Name = "minimizeBtn";
+            this.minimizeBtn.PressedColor = System.Drawing.Color.White;
+            this.minimizeBtn.ShadowDecoration.Parent = this.minimizeBtn;
+            this.minimizeBtn.Size = new System.Drawing.Size(45, 27);
+            this.minimizeBtn.TabIndex = 27;
+            this.minimizeBtn.Click += new System.EventHandler(this.minimizeBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.BackColor = System.Drawing.Color.Transparent;
+            this.exitBtn.BorderColor = System.Drawing.Color.Transparent;
+            this.exitBtn.BorderRadius = 4;
+            this.exitBtn.BorderThickness = 2;
+            this.exitBtn.CheckedState.Parent = this.exitBtn;
+            this.exitBtn.CustomBorderColor = System.Drawing.Color.Transparent;
+            this.exitBtn.CustomImages.Parent = this.exitBtn;
+            this.exitBtn.FillColor = System.Drawing.Color.Transparent;
+            this.exitBtn.Font = new System.Drawing.Font("Sitka Text", 11.25F);
+            this.exitBtn.ForeColor = System.Drawing.Color.White;
+            this.exitBtn.HoveredState.Parent = this.exitBtn;
+            this.exitBtn.Image = ((System.Drawing.Image)(resources.GetObject("exitBtn.Image")));
+            this.exitBtn.ImageSize = new System.Drawing.Size(12, 12);
+            this.exitBtn.Location = new System.Drawing.Point(819, 3);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.PressedColor = System.Drawing.Color.White;
+            this.exitBtn.ShadowDecoration.Parent = this.exitBtn;
+            this.exitBtn.Size = new System.Drawing.Size(45, 27);
+            this.exitBtn.TabIndex = 26;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // CodeEditor
+            // 
+            this.CodeEditor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CodeEditor.Location = new System.Drawing.Point(0, 32);
+            this.CodeEditor.MinimumSize = new System.Drawing.Size(20, 20);
+            this.CodeEditor.Name = "CodeEditor";
+            this.CodeEditor.Size = new System.Drawing.Size(867, 569);
+            this.CodeEditor.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 24);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // EditorWndw
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(13)))), ((int)(((byte)(13)))));
+            this.ClientSize = new System.Drawing.Size(867, 601);
+            this.Controls.Add(this.CodeEditor);
+            this.Controls.Add(this.topPnl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "EditorWndw";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DreamWriter";
+            this.Load += new System.EventHandler(this.EditorWndw_Load);
+            this.topPnl.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Siticone.UI.WinForms.SiticoneDragControl dragCtrl;
+        private System.Windows.Forms.Panel topPnl;
+        public Siticone.UI.WinForms.SiticoneButton exitBtn;
+        public Siticone.UI.WinForms.SiticoneButton minimizeBtn;
+        private System.Windows.Forms.WebBrowser CodeEditor;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
